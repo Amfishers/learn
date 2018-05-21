28fe103fb85a3735a7f0e7313262c965c9cad9a1
Fisher <378437235@qq.com>
2018-05-22 00:20:42 +0800

    [+]之前敲的一些代码

---
+++ front-end-engineering/php-learn/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/25
+ * Time: 23:38
+ */
+
+
+    class Person {
+        constructor(){
+
+        }
+    }
+    $a = 10;
+    echo $a;
+
+
+?>

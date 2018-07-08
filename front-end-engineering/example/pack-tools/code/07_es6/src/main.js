50832615ba84224edeb24e70a7b6bce4a1c17a12
Fisher <378437235@qq.com>
2018-07-09 01:04:30 +0800

    [+]babel 编译

---
+++ front-end-engineering/example/pack-tools/code/07_es6/src/main.js
@@ -0,0 +1,5 @@
+let a;
+const b = 7;
+
+//我是es6  看一下我在不
+let sc = new Promise();
---
+++ front-end-engineering/example/pack-tools/code/07_es6/webpack.build.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: {
+        //可以有多个入口，也可以有一个，如果一个，就默认从这一个入口分析
+        // main为不固定写法
+        'main': './src/main.js'
+    },
+    //输出
+    output: {
+        path: path.resolve('./dist'),  //path的一个api，相对转绝对
+        filename: './build.js'
+    },
+    module: {
+        loaders: [
+            { 
+                //安装 css-loader style-loader 包
+                test: /\.css$/,
+                loader: 'style-loader!css-loader' 
+            },
+            {
+                //安装包：  file-loader  url-loader
+                //limit 字节大小，如果图片大于限定字节就会多出一张图片
+                test: /\.(jpg|png|gif|svg)$/,
+                loader: 'url-loader?limit=4096'   
+            }, 
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+            {
+                //依赖包babel-core babel-loader babel-preset-env babel-plugin-transform-runtime
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,   //排除node_modules包，否则会报错
+                options: {
+                    presets: ['env'],
+                    plugins: ['transform-runtime']
+                }
+            }
+        ]
+    },
+    //监控
+    plugins: [
+        //插件执行顺序与元素索引有关
+        new HtmlWebpackPlugin({
+            template:'./src/index.html', //参照物
+        })
+    ]
+}

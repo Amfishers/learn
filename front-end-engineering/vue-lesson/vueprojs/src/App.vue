03b2902bda299b9f0204b222fd9f5748c11db646
fisher <378437235@qq.com>
2018-08-01 14:52:38 +0800

    [+]修改轮播图
    [+]修改轮播图

--- front-end-engineering/vue-lesson/vueprojs/src/App.vue
+++ front-end-engineering/vue-lesson/vueprojs/src/App.vue
@@ -51,4 +51,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+html,
+body {
+  margin: 0;
+}
 </style>
--- front-end-engineering/vue-lesson/vueprojs/src/components/Home/index.vue
+++ front-end-engineering/vue-lesson/vueprojs/src/components/Home/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <mt-swipe :auto="4000">
-        <mt-swipe-item v-for="img in imgs">
-            <img :src="img.img" />
-        </mt-swipe-item>
-    </mt-swipe>
-  </div>
+    <div class="page-swipe">
+        <mt-swipe :auto="4000">
+            <mt-swipe-item v-for="(item,index) in imgs" :key="index">
+                <img :src="item.img" />
+            </mt-swipe-item>
+        </mt-swipe>
+    </div>
 </template>
 
 <script>
@@ -25,5 +25,27 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+/* scoped属性，将样式私有化 */
+.page-swipe {
+    width: 100%;
+}
+.page-swipe .mint-swipe {
+    height: 280px;
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+    margin-bottom: 20px;
+}
+.page-swipe .mint-swipe-item {
+    line-height: 200px;
+}
+.page-swipe .mint-swipe-item img {
+    width: 100%;
+}
+.page-swipe-desc {
+    text-align: center;
+    color: #666;
+    margin-bottom: 5px;
+}
 </style>

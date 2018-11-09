2f7aa6bd25d096c31510385ce2519fa207b3ee5f
fisher <378437235@qq.com>
2018-11-09 16:12:21 +0800

    [*]搞定静态资源路由

--- front-end-engineering/my-music/app.js
+++ front-end-engineering/my-music/app.js
@@ -1,14 +1,28 @@
 const Koa = require('koa')
 const render = require('koa-art-template')
 const path = require('path')
+const koaStatic = require('koa-static')  // 路由解析
 const app = new Koa()
 
-
+// 渲染 koa-art-template 模板
 render(app, {
     root: path.join(__dirname, 'views'),
     extname: '.html',
     debug: process.env.NODE_ENV !== 'production'
 });
+
+// 强行修改静态资源路径
+app.use(async (ctx, next) => {
+    // 判断，如果当前请求是以/public开头，重写其url再放行
+    if( ctx.request.url.startsWith('/public') ) {
+        ctx.request.url = ctx.request.url.replace(/^\/public/, '')
+        // /public/static/css => /static/css
+    }
+    // else 如果不是/public开头，统一放行
+    await next()
+})
+app.use(koaStatic('./public'))
+
 
 let router = require('./routes/user_router')
 

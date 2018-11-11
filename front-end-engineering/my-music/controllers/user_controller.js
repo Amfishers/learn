fc54ce6c51d76bea1ffa00bb51cda693c9ba33b9
fisher <378437235@qq.com>
2018-11-12 00:39:54 +0800

    [*]用户注册重复逻辑

--- front-end-engineering/my-music/controllers/user_controller.js
+++ front-end-engineering/my-music/controllers/user_controller.js
@@ -23,6 +23,18 @@ exports.doRegister = async (ctx) => {
     let { username, password, email } = ctx.request.body
     // 2.查询
     let users = await user_db.sqlQuery('SELECT * FROM users where username = ? or email = ?', [username, email])
-    console.log(users)
+    let user
+    if (users.length !== 0) {
+        if (users.length > 1) return ctx.body = {code: '004', msg: '用户名和邮箱都存在'}
+
+        user = users[0]
+        if (user.username === username && user.emial === email) return ctx.body = { code: '004', msg: '用户名和邮箱都存在'}
+        if (user.username === username) return  ctx.body = { code: '002', msg: '用户名已经存在'}
+        if (user.email === email) return ctx.body = { code: '003', msg: '邮箱已存在'}
+    }
+    // user.length == 0 完全不等 => 可以注册
+    // 插入数据
+    let result = await user_db.sqlQuery('INSERT INTO users (username, password, email) values (?,?,?)', [username, password, email])
+    ctx.body = { code: '001', msg: '注册成功'}
 }
 
